9d5359a9922077aca6a1c2139c709c7bc89f7d61
SemiPogi <isolento92@gmial.com>
2026-05-27 13:32:27 +0800

    feat(profile): bento layout for edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,17 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    <x-page-header title="Profile" subtitle="Manage your account information and password"/>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-5xl">
+        <x-bento-card class="lg:col-span-2">
+            @include('profile.partials.update-profile-information-form')
+        </x-bento-card>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+        <x-bento-card>
+            @include('profile.partials.update-password-form')
+        </x-bento-card>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+        <x-bento-card class="lg:col-span-3 border border-danger/20 bg-rose-50/50">
+            @include('profile.partials.delete-user-form')
+        </x-bento-card>
     </div>
 </x-app-layout>
